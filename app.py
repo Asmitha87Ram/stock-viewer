4bd4c4f4589226fdde96234cd8970d83a3160309
Asmitha Ram <asmitharam87@gmail.com>
2026-04-17 10:33:58 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+stock = input("Enter stock symbol (e.g., AAPL, TCS.NS): ")
+
+data = yf.download(stock, period="7d")
+
+plt.plot(data['Close'])
+plt.title(f"{stock} Stock Price (Last 7 Days)")
+plt.xlabel("Date")
+plt.ylabel("Price")
+plt.show()
+
+highest_price = data['High'].max()
+lowest_price = data['Low'].min()
+
+start_price = data['Close'][0]
+end_price = data['Close'][-1]
+
+percentage_change = ((end_price - start_price) / start_price) * 100
+
+print("Highest Price:", round(highest_price, 2))
+print("Lowest Price:", round(lowest_price, 2))
+print("Percentage Change:", round(percentage_change, 2), "%")
